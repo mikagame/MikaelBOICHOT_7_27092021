e8ab37dd0aa81fc3534997f45ae97fe7e3aba902
Mika <Prisca@mika-2.home>
2021-09-29 14:42:09 +0200

    signup

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -4,10 +4,21 @@ import Navigation from '../components/Navigation';
 
 const Signup = () => {
     return (
-        <div>
+        <div className="signup">
              <Navigation />
             <h1>Signup</h1>
-            
+            <form>
+                <label htmlFor="email">E-mail :</label>
+                <input type="email" id="email" name="email"/>
+                
+                <label htmlFor="userName">Username :</label>
+                <input type="text" id="userName" name="userName"/>
+                
+                <label htmlFor="password">Password :</label>
+                <input type="text" id="password" name="password"/>
+                
+                <button>Signup</button>
+            </form>
         </div>
     );
 };
