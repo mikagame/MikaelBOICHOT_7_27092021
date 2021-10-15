a090415b85dc00e540f1684a909df73f0189cb5d
Mika <Prisca@mika-2.home>
2021-10-15 15:18:28 +0200

    correction taille du poste

--- backend/models/Wall.js
+++ backend/models/Wall.js
@@ -2,7 +2,7 @@ module.exports = (sequelize, DataTypes) => {
     const Wall = sequelize.define('Wall', {
         id: {type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true},
         userId: {type: DataTypes.STRING(255), allowNull: false},
-        post: {type: DataTypes.STRING(255), allowNull: false},
+        post: {type: DataTypes.TEXT, allowNull: false},
         imgUrl: {type: DataTypes.STRING(255), allowNull: true},
         username: {type: DataTypes.STRING(255), allowNull: false},
     },
